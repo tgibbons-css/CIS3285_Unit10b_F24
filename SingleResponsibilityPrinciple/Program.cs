64c20499a630542b27e495080df88d8e204910d4
Tom Gibbons <tgibbons@css.edu>
2024-10-27 20:49:48 -0500

    Add project files.

---
+++ SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+
+using SingleResponsibilityPrinciple.AdoNet;
+
+namespace SingleResponsibilityPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+            String fileName = "SingleResponsibilityPrinciple.trades.txt";
+            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(fileName);
+            if (tradeStream == null)
+            {
+                logger.LogWarning("trade file could not be openned at " + fileName);
+                Environment.Exit(1); // Exits the application with a non-zero exit code indicating an error
+            }
+
+            var tradeValidator = new SimpleTradeValidator(logger);
+            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var tradeMapper = new SimpleTradeMapper();
+            var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
+            var tradeStorage = new AdoNetTradeStorage(logger);
+
+            var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            tradeProcessor.ProcessTrades();
+
+            Console.ReadKey();
+        }
+    }
+}
